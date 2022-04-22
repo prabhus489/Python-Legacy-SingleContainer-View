c76662f2babedbbb360875666809ca5110848fd6
Prabhu Sundarraj <prabhu12yuva@gmail.com>
2022-04-23 03:10:14 +0530

    created dockerfile to build image for hosing single container and using this single comtainer our both(all) applications are running

--- FlaskPgSharedDB.py
+++ FlaskPgSharedDB.py
@@ -107,4 +107,6 @@ def sendin(db=connect()):
  
  
 
-app.run(host='localhost', port=5000) 
+#app.run(host='localhost', port=5000) 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ startmyapps.sh
@@ -0,0 +1,4 @@
+nohup python FlaskPgSharedDB.py > flaskapp.log &
+#nohup python manage.py runserver 0.0.0.0:8000 > djangoapp.log &
+#python FlaskPgSharedDB.py
+python manage.py runserver 0.0.0.0:8000
